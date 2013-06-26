73fc580520ea9037c0b1acf0fd6ea45ea00182bf
fmllr <svefre@gmail.com>
2013-06-26 19:22:59 +0200

    Improved link to CSS file in layout file

--- app/view/layout/default.tpl.php
+++ app/view/layout/default.tpl.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title><?php echo $title; ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo Config::get('app.base_url'); ?>public/css/TinyMVC.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo url(); ?>public/css/TinyMVC.css">
 </head>
 <body>
     <div id="wrapper">
