7afce30ebbbdf88b427a8ddb3ada2c80a89a8cef
fmllr <fmllr.com@gmail.com>
2013-08-18 19:28:38 +0200

    Using __autoload function instead of lots of require_once

--- app/lib/init.php
+++ app/lib/init.php
@@ -3,13 +3,17 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once ABS_PATH . 'app/lib/config.php';
 require_once ABS_PATH . 'app/lib/functions.php';
-require_once ABS_PATH . 'app/lib/request.php';
-require_once ABS_PATH . 'app/lib/router.php';
-require_once ABS_PATH . 'app/lib/db.php';
-require_once ABS_PATH . 'app/lib/view.php';
-require_once ABS_PATH . 'app/lib/plugin.php';
+
+function __autoload($class) {
+    $class = ABS_PATH . 'app/lib/' . strtolower($class) . '.php';
+
+    if (!is_readable($class)) {
+        return;
+    }
+
+    require_once $class;
+}
 
 if (Config::get('app.tinymvc_dashboard_url')) {
     Router::register(Config::get('app.tinymvc_dashboard_url'), 'tinymvc_dashboard@index');
