bb103b7f874e2611a82a1294bcaa9c7b9901f1e3
Freddy <f.mueller@loom-berlin.com>
2013-08-28 15:13:12 +0200

    Removed unnecessary reference.

--- app/plugin/meta/plugin.php
+++ app/plugin/meta/plugin.php
@@ -1,6 +1,6 @@
 <?php
 
-function meta__layout_render(&$data) {
+function meta__layout_render($data) {
     $data['meta_keywords'] = '<meta name="keywords" content="php, mvc, tinymvc, framework">';
     $data['meta_description'] = '<meta name="description" content="TinyMVC is a small PHP-based MVC-Framework.">';
 
