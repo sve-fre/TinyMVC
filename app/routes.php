f54d40db0ebe72d67dffac6dac191257555275ef
fmllr <svefre@gmail.com>
2013-09-06 22:36:08 +0200

    Modified demo content.

--- app/routes.php
+++ app/routes.php
@@ -6,11 +6,19 @@ Router::register('home/about/lala', 'home@lala');
 Router::route('no-controller', function() {
     $title = title('No controller');
     $sub_headline = 'No controller was used to display this content';
+    $breadcrumb = App::breadcrumb();
 
     $data = array(
         'title' => $title,
         'header' => View::render('header', null, array('sub_dir' => 'misc')),
-        'content' => View::render('index', array('sub_headline' => $sub_headline), array('sub_dir' => 'home'))
+        'content' => View::render(
+            'index',
+            array(
+                'sub_headline' => $sub_headline,
+                'breadcrumb' => $breadcrumb
+            ),
+            array('sub_dir' => 'home')
+        )
     );
 
     View::layout('default', $data);
