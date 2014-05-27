9351be8fafccb77e5e7b15370e794647c1ddec62
S.-Frederick Müller <svefre@gmail.com>
2014-05-27 18:47:08 +0200

    Removed realpath() from ABS_PATH.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
 define('DS', DIRECTORY_SEPARATOR);
-define('ABS_PATH', realpath(dirname(__FILE__)) . DS);
+define('ABS_PATH', dirname(__FILE__) . DS);
 
 require_once ABS_PATH . 'app/init.php';
