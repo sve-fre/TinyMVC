93960e2050c7337ffa5bad6beee6f427305e9bcf
S.-Frederick Müller <svefre@gmail.com>
2014-06-15 20:17:00 +0200

    Removed class Breadcrumb.

--- app/controller/base_controller.php
+++ app/controller/base_controller.php
@@ -3,14 +3,9 @@
 class base_controller {
 
     protected static $layout = null;
-    protected static $breadcrumb_config = array();
 
     public function __construct() {
         self::$layout = 'default';
-        self::$breadcrumb_config = array(
-            'show_home' => true,
-            'home_title' => 'Home'
-        );
     }
 
 }
