2475968abcb9e268a9afeb9116eb81ae9c8c51a5
fmllr <fmllr.com@gmail.com>
2014-06-15 19:06:45 +0200

    Deleted model Lorem.

--- app/model/Lorem.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class Lorem {
-
-    public static function getMessage() {
-        return 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.';
-    }
-
-}
