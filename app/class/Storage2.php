057816579c0dd1168257976d9587d916fcac9e1a
Sven-Frederick Müller <svefre@gmail.com>
2014-06-18 12:36:43 +0200

    Updated class Storage2.

--- app/class/Storage2.php
+++ app/class/Storage2.php
@@ -2,12 +2,14 @@
 
 class Storage2 {
 
-    private $_storages = array();
+    private static $_storages = array();
     private static $_dir = '';
     private static $_extension = '';
     private static $_separator = '';
     private static $_init = false;
-    private $_instance = null;
+    private static $_instance = null;
+    private static $_where = array();
+    private static $_storage = null;
 
 
     private static function _init() {
@@ -29,12 +31,11 @@ class Storage2 {
             return false;
         }
 
-
         if (!in_array('id', $columns)) {
             array_unshift($columns, 'id');
         }
 
-        $columns = implode('|', $columns);
+        $columns = implode('|', $columns) . "\n";
         File::make($storage);
 
         return (File::write($storage, $columns)) ? true : false;
@@ -51,23 +52,66 @@ class Storage2 {
             return false;
         }
 
-        return file(self::_file($storage));
+        if (!isset(self::$_storages[$storage])) {
+            self::$_storages[$storage] = file(self::_file($storage));
+        }
+
+        return self::$_storages[$storage];
     }
 
 
-    public function insert($storage, $data) {
-        if (!self::exists($storage) || !is_array($data)) {
+    public static function insert($storage, $data) {
+        if (!self::exists($storage) || !is_array($data) || !is_array(self::_read($storage)) || !count(self::_read($storage))) {
             return false;
         }
+
+        $storage_data = self::_read($storage);
+        $column_names = explode(self::$_separator, $storage_data[0]);
+        $last_id = explode(self::$_separator, $storage_data[count($storage_data) - 1])[0];
+        $id = (is_numeric($last_id)) ? $last_id + 1 : 1;
+        $tmp_data = array();
+
+        foreach ($data as $column => $value) {
+            if (in_array($column, $column_names)) {
+                $index = array_keys($column_names, $column)[0];
+                $tmp_data[$index] = ($column === 'id') ? $id : $value;
+            }
+        }
+
+        ksort($tmp_data);
+        File::append(self::_file($storage), implode(self::$_separator, $tmp_data). "\n");
     }
 
 
-    public static function get() {
-        if (!isset(self::$_instance)) {
+    public function where($column, $operator, $value) {
+        self::$_where[] = array(
+            'column' => $column,
+            'operator' => $operator,
+            'value' => $value
+        );
+
+        return $this;
+    }
+
+
+    public static function get($storage) {
+        if (self::$_instance === null) {
             self::$_instance = new self;
         }
 
+        self::$_where = array();
+        self::$_storage = self::_read($storage);
+
         return self::$_instance;
+    }
+
+
+    public static function all($storage) {
+        if (!self::exists($storage)) {
+            return false;
+        }
+
+        return (isset(self::$_storages[$storage])) ? self::$_storages[$storage] : self::$_read($storage);
     }
 
 
